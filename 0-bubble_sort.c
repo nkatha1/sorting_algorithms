2015284b0a47f0363c3d2aa6140659ff7003e7ff
nkatha1 <nkathapatience1@gmail.com>
2024-01-27 17:09:53 +0300

    sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,33 +1,46 @@
 #include "sort.h"
-#include <stdbool.h>
-/**
- * bubble_sort - sorts an int array in ascending order using Bubble sort algo
- * @array: pointer to the given set of data
- * @size: size of the array given
- * Return: void
- */
 
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	int temp;
-	bool swap;
+	size_t i, len = size;
+	bool bubbly = false;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
 
-	do {
-		swap = false;
-		for (i = 0; i < size - 1; i++)
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				swap = true;
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
+				bubbly = false;
 			}
 		}
-	} while (swap);
+		len--;
+	}
 }
